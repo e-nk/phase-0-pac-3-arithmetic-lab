6d5fa062829b63ec966979c589cf6cb48c1ab57d
e-nk <enocklagat747@gmail.com>
2022-11-18 13:54:55 +0300

    DONE WITH ASSIGNMENT

---
+++ index.js
@@ -0,0 +1,37 @@
+var a, b;
+function add(a, b) {
+    return a + b
+}
+function subtract(a, b){
+    return a - b
+
+
+}
+function multiply(a, b){
+    return a * b
+
+
+}
+function divide(a, b){
+    return a / b
+
+
+}
+function increment(n){
+    return n + 1
+
+}
+function decrement(n){
+    return n - 1
+
+}
+function makeInt(n, base){
+    parseInt(n, 10);
+    return n, parseInt(n, 10);
+
+}
+function preserveDecimal(n){
+       parseFloat (n);
+    return parseFloat (n) 
+}
+
